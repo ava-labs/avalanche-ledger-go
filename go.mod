bebf2df5baef586f7f6057e99083cc6a898dc68b
Patrick O'Grady <prohb125@gmail.com>
2022-02-17 18:02:36 -0800

    stuck on error 6a12

--- go.mod
+++ go.mod
@@ -1,3 +1,18 @@
 module github.com/ava-labs/avalanche-ledger-go
 
 go 1.17
+
+require (
+	github.com/djimenez/iconv-go v0.0.0-20160305225143-8960e66bd3da // indirect
+	github.com/ethereum/go-ethereum v1.10.16 // indirect
+	github.com/golang/protobuf v1.4.3 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/zondax/hid v0.9.0 // indirect
+	github.com/zondax/ledger-go v0.12.2 // indirect
+	golang.org/x/net v0.0.0-20210805182204-aaa1db679c0d // indirect
+	golang.org/x/sys v0.0.0-20210816183151-1e6c022a8912 // indirect
+	golang.org/x/text v0.3.6 // indirect
+	google.golang.org/genproto v0.0.0-20200108215221-bd8f9a0ef82f // indirect
+	google.golang.org/grpc v1.29.1 // indirect
+	google.golang.org/protobuf v1.23.0 // indirect
+)
--- main.go
+++ main.go
@@ -1,7 +1,64 @@
 package main
 
-import "fmt"
+import (
+	"encoding/binary"
+	"fmt"
+
+	"github.com/djimenez/iconv-go"
+	"github.com/zondax/ledger-go"
+)
+
+const (
+	CLA                   = 0x80
+	INS_PROMPT_PUBLIC_KEY = 0x02
+)
+
+func GetBip32bytes(bip32Path []uint32, hardenCount int) ([]byte, error) {
+	message := make([]byte, 41)
+	if len(bip32Path) > 10 {
+		return nil, fmt.Errorf("maximum bip32 depth = 10")
+	}
+	message[0] = byte(len(bip32Path))
+	for index, element := range bip32Path {
+		pos := 1 + index*4
+		value := element
+		if index < hardenCount {
+			value = 0x80000000 | element
+		}
+		binary.LittleEndian.PutUint32(message[pos:], value)
+	}
+	return message, nil
+}
 
 func main() {
 	fmt.Println("vim-go")
+	admin := ledger_go.NewLedgerAdmin()
+	fmt.Println(admin.ListDevices())
+	fmt.Println(admin.CountDevices())
+	device, err := admin.Connect(0)
+	if err != nil {
+		panic(err)
+	}
+	msg := []byte{
+		CLA,
+		INS_PROMPT_PUBLIC_KEY,
+		0x04,
+		0x00,
+	}
+	data := []byte{}
+	output, err := iconv.ConvertString("fuji", "utf-8", "latin1")
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(output)
+	data = append(data, output...)
+	bip32Bytes, err := GetBip32bytes([]uint32{44, 9000, 0, 0, 0}, 3)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(bip32Bytes)
+	data = append(data, bip32Bytes...)
+	msg = append(msg, byte(len(data)))
+	msg = append(msg, data...)
+	fmt.Println(device.Exchange(msg))
 }
