81aa9b1acec6b3a6a54fb128685767992b062844
Patrick O'Grady <prohb125@gmail.com>
2022-02-20 17:02:56 -0800

    more progress

--- main.go
+++ main.go
@@ -16,10 +16,9 @@ const (
 	INS_PROMPT_PUBLIC_KEY = 0x02
 	INS_SIGN_HASH         = 0x04
 	HRP                   = "fuji"
-	HARDEN_COUNT          = 3
 )
 
-func bip32bytes(bip32Path []uint32) ([]byte, error) {
+func bip32bytes(bip32Path []uint32, hardenCount int) ([]byte, error) {
 	message := make([]byte, 1+len(bip32Path)*4)
 	if len(bip32Path) > 10 {
 		return nil, fmt.Errorf("maximum bip32 depth = 10")
@@ -28,12 +27,42 @@ func bip32bytes(bip32Path []uint32) ([]byte, error) {
 	for index, element := range bip32Path {
 		pos := 1 + index*4
 		value := element
-		if index < HARDEN_COUNT {
+		if index < hardenCount {
 			value = 0x80000000 | element
 		}
 		binary.BigEndian.PutUint32(message[pos:], value)
 	}
 	return message, nil
+}
+
+func collectSignaturesFromSuffixes(device ledger_go.LedgerDevice, prefix []uint32, suffixes [][]uint32) [][]byte {
+	results := make([][]byte, len(suffixes))
+	for i := 0; i < len(suffixes); i++ {
+		suffix := suffixes[i]
+		p1 := 0x01
+		if i == len(suffixes)-1 {
+			p1 = 0x81
+		}
+		fmt.Println("signing:", append(prefix, suffix...))
+		data, err := bip32bytes(suffix, 0)
+		if err != nil {
+			panic(err)
+		}
+		msgSig := []byte{
+			CLA,
+			INS_SIGN_HASH,
+			byte(p1),
+			0x0,
+		}
+		msgSig = append(msgSig, byte(len(data)))
+		msgSig = append(msgSig, data...)
+		sig, err := device.Exchange(msgSig)
+		if err != nil {
+			panic(err)
+		}
+		results[i] = sig[:len(sig)-2]
+	}
+	return results
 }
 
 func main() {
@@ -71,7 +100,7 @@ func main() {
 		0x0,
 	}
 	data := []byte(HRP)
-	pathBytes, err := bip32bytes([]uint32{44, 9000, 0, 0, 0})
+	pathBytes, err := bip32bytes([]uint32{44, 9000, 0, 0, 0}, 3)
 	if err != nil {
 		panic(err)
 	}
@@ -95,10 +124,11 @@ func main() {
 	// TODO: Get Extended Public Key to get all UTXOs
 
 	// Sign Hash
-	data = []byte{0x1}
+	suffixes := [][]uint32{{0, 1}, {0, 3}}
+	data = []byte{byte(len(suffixes))}
 	rawHash := hashing.ComputeHash256([]byte{0x1, 0x2, 0x3, 0x4})
 	data = append(data, rawHash...)
-	pathBytes, err = bip32bytes([]uint32{44, 9000, 0})
+	pathBytes, err = bip32bytes([]uint32{44, 9000, 0}, 3)
 	if err != nil {
 		panic(err)
 	}
@@ -121,6 +151,10 @@ func main() {
 	fmt.Printf("signed hash: %x\n", rawHash)
 
 	// Get Signatures
+	sigs := collectSignaturesFromSuffixes(device, []uint32{44, 9000, 0}, suffixes)
+	for i, sig := range sigs {
+		fmt.Printf("sigs (%v): %x\n", suffixes[i], sig)
+	}
 
 	// TODO: Sign Transaction
 	// PVM: https://github.com/ava-labs/avalanchego/blob/f0a3bbb7d745be99d4970fb3b8fba3c7da87b891/vms/platformvm/tx.go#L100-L129
