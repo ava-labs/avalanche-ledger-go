da4e27f09b0814f4d18fc473fb08b475fae8adf3
Patrick O'Grady <prohb125@gmail.com>
2022-02-20 17:04:49 -0800

    cleanup

--- main.go
+++ main.go
@@ -124,11 +124,12 @@ func main() {
 	// TODO: Get Extended Public Key to get all UTXOs
 
 	// Sign Hash
+	prefix := []uint32{44, 9000, 0}
 	suffixes := [][]uint32{{0, 1}, {0, 3}}
 	data = []byte{byte(len(suffixes))}
 	rawHash := hashing.ComputeHash256([]byte{0x1, 0x2, 0x3, 0x4})
 	data = append(data, rawHash...)
-	pathBytes, err = bip32bytes([]uint32{44, 9000, 0}, 3)
+	pathBytes, err = bip32bytes(prefix, 3)
 	if err != nil {
 		panic(err)
 	}
@@ -148,12 +149,12 @@ func main() {
 	if !bytes.Equal(responseHash, rawHash) {
 		panic("signed hash changed")
 	}
-	fmt.Printf("signed hash: %x\n", rawHash)
+	fmt.Printf("message hash: %x\n", rawHash)
 
 	// Get Signatures
-	sigs := collectSignaturesFromSuffixes(device, []uint32{44, 9000, 0}, suffixes)
+	sigs := collectSignaturesFromSuffixes(device, prefix, suffixes)
 	for i, sig := range sigs {
-		fmt.Printf("sigs (%v): %x\n", suffixes[i], sig)
+		fmt.Printf("sigs (%v): %x\n", append(prefix, suffixes[i]...), sig)
 	}
 
 	// TODO: Sign Transaction
