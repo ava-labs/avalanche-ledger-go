b32744842cda98724acebe45f49b5760a3b1635f
Felipe Madero <felipe.madero@gmail.com>
2022-11-08 16:31:26 -0300

    lint

--- ledger_test.go
+++ ledger_test.go
@@ -34,7 +34,7 @@ func TestLedger(t *testing.T) {
 	fmt.Printf("address: %+v\n", address)
 
 	// Get Extended Addresses
-	addresses, err := device.Addresses([]uint32{0,1,2,3,4,5,6,7,8,9,10})
+	addresses, err := device.Addresses([]uint32{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10})
 	if err != nil {
 		t.Fatal(err)
 	}
